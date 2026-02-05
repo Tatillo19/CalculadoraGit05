2afd042b0f727931025a43f90f5485d77174aba3
Raúl Cumbreño <tatillo2007@gmail.com>
2026-02-05 13:23:46 +0100

    Proyecto CalculadoraGIT05 clase Calculadora y método potencia y módulo

--- src/calculadora05/Calculadora05.java
+++ src/calculadora05/Calculadora05.java
@@ -18,4 +18,14 @@ public class Calculadora05 {
 	    }
 	    return (double) a / b;
 	}
+	public int potencia(int base, int exponente) {
+		 int resultado = 1;
+		 for (int i = 0; i < exponente; i++) {
+		 resultado *= base;
+		 }
+		 return resultado;
+		}
+		public int modulo(int a, int b) {
+		 return a % b;
+		}
 }
