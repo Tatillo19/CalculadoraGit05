8527637513489935c0908fa420468c96d4dd3b5d
Raúl Cumbreño <tatillo2007@gmail.com>
2026-02-05 12:56:04 +0100

    Proyecto CalculadoraGIT05 clase Calculadora y método multiplicar y dividir

--- src/calculadora05/Calculadora05.java
+++ src/calculadora05/Calculadora05.java
@@ -8,4 +8,11 @@ public class Calculadora05 {
 	public int restar(int a, int b) {
 	    return a - b;
 	}
+	public int multiplicar(int a, int b) {
+	    return a * b;
+	}
+
+	public double dividir(int a, int b) {
+	    return (double) a / b;
+	}
 }
